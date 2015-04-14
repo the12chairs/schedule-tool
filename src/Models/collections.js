5b4d81304747e5332655d4a5c8bb51f76aa8af17
Maksim Bondarenko <maksim.bondarenko@noveogroup.com>
2015-04-14 16:16:32 +0600

    xls export

--- src/Models/collections.js
+++ src/Models/collections.js
@@ -282,6 +282,28 @@ Schemas.Schedule = new SimpleSchema({
     }
 })
 
+temporaryFiles = new FileCollection('temporaryFiles',
+{
+    resumable: false,   // Enable built-in resumable.js upload support
+    http: [{
+         method: 'get',
+        path: '/:_id',  // this will be at route "/gridfs/temporaryFiles/:_id"
+        lookup: function (params) {  // uses express style url params
+            return { _id: params._id};       // a query mapping url to myFiles
+        }
+    },
+    {
+        method: 'post',
+        path: '/:_id',
+        lookup: function (params) {
+            return {
+                _id: params._id
+            }
+        }
+    }
+    ]}
+);
+
 chairs = new Mongo.Collection('chairs')
 chairs.attachSchema(Schemas.Chair)
 
--- src/schedule-tool.js
+++ src/schedule-tool.js
@@ -229,6 +229,20 @@ if (Meteor.isClient) {
         }
     })
 
+    Template.listGroups.events({
+        'click #downloadSchedule': function(event) {
+            var groupName = this.name
+
+            Meteor.call('downloadExcelFile', groupName, function(err, fileUrl) {
+                var link = document.createElement("a");
+                link.download = 'Расписание.xlsx';
+                link.href = fileUrl;
+                link.click();
+            });
+
+        }
+    });
+
     Template.listSchedules.events({
         'click #doFilter': function(event, template) {
             event.preventDefault();
@@ -273,7 +287,7 @@ if (Meteor.isClient) {
             $('select[name="room"]').val('')
             $('select[name="lecturer"]').val('')
             $('select[name="group"]').val('')
-        }
+        },
     });
 
 
@@ -323,7 +337,246 @@ if (Meteor.isServer) {
         Roles.addUsersToRoles(usr2, ['viewer'])
     }
 
+    Meteor.methods({
+        downloadExcelFile : function(groupName) {
+            var Future = Npm.require('fibers/future');
+            var futureResponse = new Future();
+
+            var excel = new Excel('xlsx'); // Create an excel object  for the file you want (xlsx or xls)
+            var workbook = excel.createWorkbook(); // Create a workbook (equivalent of an excel file)
+            var worksheet = excel.createWorksheet(); // Create a worksheet to be added to the workbook
+            worksheet.writeToCell(0,0, 'Расписание группы ' + groupName); // Example : writing to a cell
+
+            var sched = [];
+
+            schedules.find({'group.name': groupName}).forEach(function(s) {
+                if(s.weekday == 'Понедельник') {
+                    sched.push({
+                        'mon': {
+                            pair: s.pair,
+                            body: s.subject.title + ", " + s.room.number + " ("+ s.room.building +") " + s.lecturer.fio,
+                            num: s.weeknumber
+                        }
+                    });
+                } else
+                if(s.weekday == 'Вторник') {
+                    sched.push({
+                        'tue': {
+                            pair: s.pair,
+                            body: s.subject.title + ", " + s.room.number + " ("+ s.room.building +") " + s.lecturer.fio,
+                            num: s.weeknumber
+                        }
+                    });
+                } else
+                if(s.weekday == 'Среда') {
+                    sched.push({
+                        'wed': {
+                            pair: s.pair,
+                            body: s.subject.title + ", " + s.room.number + " ("+ s.room.building +") " + s.lecturer.fio,
+                            num: s.weeknumber
+                        }
+                    });
+                } else
+                if(s.weekday == 'Четверг') {
+                    sched.push({
+                        'thu': {
+                            pair: s.pair,
+                            body: s.subject.title + ", " + s.room.number + " ("+ s.room.building +") " + s.lecturer.fio,
+                            num: s.weeknumber
+                        }
+                    });
+                } else
+                if(s.weekday == 'Пятница') {
+                    sched.push({
+                        'fri': {
+                            pair: s.pair,
+                            body: s.subject.title + ", " + s.room.number + " ("+ s.room.building +") " + s.lecturer.fio,
+                            num: s.weeknumber
+                        }
+                    });
+                } else
+                if(s.weekday == 'Суббота') {
+                    sched.push({
+                        'sat': {
+                            pair: s.pair,
+                            body: s.subject.title + ", " + s.room.number + " ("+ s.room.building +") " + s.lecturer.fio,
+                            num: s.weeknumber
+                        }
+                    });
+                }
+
+
+            /*
+                worksheet.writeToCell(row, 0, schedule.pair);
+                worksheet.writeToCell(row, 1, schedule.weekday);
+                worksheet.writeToCell(row, 2, schedule.subject.title);
+                worksheet.writeToCell(row, 3, schedule.room.number);
+                worksheet.writeToCell(row, 4, schedule.room.building);
+                worksheet.writeToCell(row, 5, schedule.lecturer.fio);
+            */
+            });
+            var monRow = 2 + 1;
+            var tueRow = 2 + 14 + 1;
+            var wedRow = 2 + 14*2 + 1;
+            var thuRow = 2 + 14*3 + 1;
+            var friRow = 2 + 14*4 + 1;
+            var satRow = 2 + 14*5 + 1;
+
+            worksheet.writeToCell(monRow, 1, 'Понедельник');
+            worksheet.writeToCell(tueRow-1, 1, 'Вторник');
+            worksheet.writeToCell(wedRow-1, 1, 'Среда');
+            worksheet.writeToCell(thuRow-1, 1, 'Четверг');
+            worksheet.writeToCell(friRow-1, 1, 'Пятница');
+            worksheet.writeToCell(satRow-1, 1, 'Суббота');
+
+            // Setting time
+
+            worksheet.writeToCell(monRow+1, 0, '8:30');
+            worksheet.writeToCell(monRow+3, 0, '10:15');
+            worksheet.writeToCell(monRow+5, 0, '12:30');
+            worksheet.writeToCell(monRow+7, 0, '14:15');
+            worksheet.writeToCell(monRow+9, 0, '16:50');
+            worksheet.writeToCell(monRow+11, 0, '18:00');
+
+            worksheet.writeToCell(tueRow, 0, '8:30');
+            worksheet.writeToCell(tueRow+2, 0, '10:15');
+            worksheet.writeToCell(tueRow+4, 0, '12:30');
+            worksheet.writeToCell(tueRow+6, 0, '14:15');
+            worksheet.writeToCell(tueRow+8, 0, '16:50');
+            worksheet.writeToCell(tueRow+10, 0, '18:00');
+
+            worksheet.writeToCell(wedRow, 0, '8:30');
+            worksheet.writeToCell(wedRow+2, 0, '10:15');
+            worksheet.writeToCell(wedRow+4, 0, '12:30');
+            worksheet.writeToCell(wedRow+6, 0, '14:15');
+            worksheet.writeToCell(wedRow+8, 0, '16:50');
+            worksheet.writeToCell(wedRow+10, 0, '18:00');
+
+            worksheet.writeToCell(thuRow, 0, '8:30');
+            worksheet.writeToCell(thuRow+2, 0, '10:15');
+            worksheet.writeToCell(thuRow+4, 0, '12:30');
+            worksheet.writeToCell(thuRow+6, 0, '14:15');
+            worksheet.writeToCell(thuRow+8, 0, '16:50');
+            worksheet.writeToCell(thuRow+10, 0, '18:00');
+
+            worksheet.writeToCell(friRow, 0, '8:30');
+            worksheet.writeToCell(friRow+2, 0, '10:15');
+            worksheet.writeToCell(friRow+4, 0, '12:30');
+            worksheet.writeToCell(friRow+6, 0, '14:15');
+            worksheet.writeToCell(friRow+8, 0, '16:50');
+            worksheet.writeToCell(friRow+10, 0, '18:00');
+
+            worksheet.writeToCell(satRow, 0, '8:30');
+            worksheet.writeToCell(satRow+2, 0, '10:15');
+            worksheet.writeToCell(satRow+4, 0, '12:30');
+            worksheet.writeToCell(satRow+6, 0, '14:15');
+            worksheet.writeToCell(satRow+8, 0, '16:50');
+            worksheet.writeToCell(satRow+10, 0, '18:00');
+
+
+            sched.forEach(function(s){
+                if(typeof(s.mon) != 'undefined') {
+                    //worksheet.writeToCell(monRow, 0, s.mon.pair);
+                    if(s.mon.num == "Первая") {
+                        worksheet.writeToCell(monRow+s.mon.pair*2-1, 1, s.mon.body);
+                    } else {
+                        worksheet.writeToCell(monRow+s.mon.pair*2, 1, s.mon.body);
+                    }
+                }
+                if(typeof(s.tue) != 'undefined') {
+                    //worksheet.writeToCell(tueRow, 0, s.tue.pair);
+                    if(s.tue.num == "Первая") {
+                        worksheet.writeToCell(tueRow+s.tue.pair*2-1-1, 1, s.tue.body);
+                    } else {
+                        worksheet.writeToCell(tueRow+s.tue.pair*2-1, 1, s.tue.body);
+                    }
+                    //tueRow++;
+                }
+                if(typeof(s.wed) != 'undefined') {
+                    //worksheet.writeToCell(wedRow, 0, s.wed.pair);
+                    if(s.wed.num == "Первая") {
+                        worksheet.writeToCell(wedRow+s.wed.pair*2-1-1, 1, s.wed.body);
+                    } else {
+                        worksheet.writeToCell(wedRow+s.wed.pair*2-1, 1, s.wed.body);
+                    }
+                    //wedRow++;
+                }
+                if(typeof(s.thu) != 'undefined') {
+                    //worksheet.writeToCell(thuRow, 0, s.thu.pair);
+                    if(s.thu.num == "Первая") {
+                        worksheet.writeToCell(thuRow+s.thu.pair*2-1-1, 1, s.thu.body);
+                    } else {
+                        worksheet.writeToCell(thuRow+s.thu.pair*2-1, 1, s.thu.body);
+                    }
+                    //thuRow++;
+                }
+                if(typeof(s.fri) != 'undefined') {
+                    //worksheet.writeToCell(friRow, 0, s.fri.pair);
+                    if(s.fri.num == "Первая") {
+                        worksheet.writeToCell(friRow+s.fri.pair*2-1-1, 1, s.fri.body);
+                    } else {
+                        worksheet.writeToCell(friRow+s.fri.pair*2-1, 1, s.fri.body);
+                    }
+                    //friRow++;
+                }
+                if(typeof(s.sat) != 'undefined') {
+                    //worksheet.writeToCell(satRow, 0, s.sat.pair);
+                    if(s.fri.num == "Первая") {
+                        worksheet.writeToCell(satRow+s.sat.pair*2-1-1, 1, s.sat.body);
+                    } else {
+                        worksheet.writeToCell(satRow+s.sat.pair*2-1, 1, s.sat.body);
+                    }
+                    //satRow++;
+                }
+
+            });
+
+
+
+            workbook.addSheet('MySheet', worksheet); // Add the worksheet to the workbook
+
+            mkdirp('tmp', Meteor.bindEnvironment(function (err) {
+                if (err) {
+                    console.log('Error creating tmp dir', err);
+                    futureResponse.throw(err);
+                }
+                else {
+                    var uuid = UUID.v4();
+                    var filePath = './tmp/' + uuid;
+                    workbook.writeToFile(filePath);
+                        temporaryFiles.importFile(filePath, {
+                        filename : uuid,
+                        contentType: 'application/octet-stream'
+                    }, function(err, file) {
+                        if (err) {
+                            futureResponse.throw(err);
+                        }
+                        else {
+                            futureResponse.return('/gridfs/temporaryFiles/' + file._id);
+                        }
+                    });
+                }
+            }));
+            sched = {};
+            return futureResponse.wait();
+        }
+    });
+
     Meteor.startup(function () {
+        temporaryFiles.allow({
+            insert: function (userId, file) {
+                return true;
+            },
+            remove: function (userId, file) {
+                return true;
+            },
+            read: function (userId, file) {
+                return true;
+            },
+            write: function (userId, file, fields) {
+                return true;
+            }
+        });
     })
 
 }
